f08a4a00655baf206d706429a5e043792388e20a
innoactive <innoactive@users.noreply.github.com>
2015-02-11 14:49:04 +0000

    Asynchronously update from DOM

--- detect-element-resize.js
+++ detect-element-resize.js
@@ -29,12 +29,15 @@
 				expand = triggers.firstElementChild,
 				contract = triggers.lastElementChild,
 				expandChild = expand.firstElementChild;
-			contract.scrollLeft = contract.scrollWidth;
-			contract.scrollTop = contract.scrollHeight;
-			expandChild.style.width = expand.offsetWidth + 1 + 'px';
-			expandChild.style.height = expand.offsetHeight + 1 + 'px';
-			expand.scrollLeft = expand.scrollWidth;
-			expand.scrollTop = expand.scrollHeight;
+			//Asynchronously update from DOM to reduce layout thrashing
+			requestFrame(function() {
+				contract.scrollLeft = contract.scrollWidth;
+				contract.scrollTop = contract.scrollHeight;
+				expandChild.style.width = expand.offsetWidth + 1 + 'px';
+				expandChild.style.height = expand.offsetHeight + 1 + 'px';
+				expand.scrollLeft = expand.scrollWidth;
+				expand.scrollTop = expand.scrollHeight;
+			});
 		};
 
 		function checkTriggers(element){
